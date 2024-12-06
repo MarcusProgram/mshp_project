87aa8248fb4de376f73f2ac1cb2baac9d41bd1dc
Marcus (Konstantin) <i_dont_think_you_need_to_know_that@gmail.com>
2024-12-06 17:24:30 +0300

    Third commit...

--- mshp_project/views.py
+++ mshp_project/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
-
+from datetime import datetime
 def index_page(request):
     context = {
         'course_name': 'Курс "Промышленное программирование"',
         'author_name': 'Константин',
-        'page_count': 1,
+        'page_count': 2,
     }
     return render(request, 'index.html', context)
+
+
+def time_page(request):
+    now = datetime.now()
+    context = {
+        'course_name': 'Курс "Промышленное программирование"',
+        'current_date': now.strftime('%d.%m.%Y'),
+        'current_time': now.strftime('%H:%M:%S'),
+    }
+    return render(request, 'time.html', context)
